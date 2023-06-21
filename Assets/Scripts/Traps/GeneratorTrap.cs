ed893a1b719d257cf8fa0dfd022fb1c8db66d05a
Phạm Minh Giang <114584785+ggiang312001@users.noreply.github.com>
2023-06-21 11:43:10 +0700

    Update Trap and renerator Trap

---
+++ Assets/Scripts/Traps/GeneratorTrap.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class GeneratorTrap : MonoBehaviour
+{
+    public GameObject WoodBox, Thorn;
+    public ThornPool ThornPool;
+    public WoodBoxPool WoodBoxPool;
+    public MinePool MinePool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float timeSpawner = 5.0f;
+    float count = 0;
+    float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeSpawner)
+        {
+            SpawnTrap();
+            timer = 0;
+        }
+        count += Time.deltaTime;
+        if (count >= 2 && (int)count % 2 == 0)
+        {
+            if (timeSpawner > 1.5)
+            {
+                timeSpawner -= 0.5f;
+            }
+            count = 0;
+        }
+    }
+
+    private void SpawnTrap()
+    {
+        List<GameObject> listThorns = ThornPool.pool;
+        List<GameObject> listWoodBoxs = WoodBoxPool.pool;
+        List<GameObject> listMine = MinePool.pool;
+        int random = UnityEngine.Random.Range(1,4);
+        if(random == 1)
+        {
+            int count = 0;
+            foreach (GameObject obj in listThorns)
+            {
+                if (obj.active == false)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                GameObject monster = ThornPool.GetObject(); // Lấy trap từ pool
+                monster.transform.position = new Vector3(transform.position.x, 2,0); // Set vị trí của trap
+                monster.SetActive(true); // Hiển thị trap lên màn hình
+                count = 0;
+            }
+        }
+        if(random == 2)
+        {
+            int count = 0;
+            foreach (GameObject obj in listWoodBoxs)
+            {
+                if (obj.active == false)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                GameObject monster = WoodBoxPool.GetObject(); // Lấy trap từ pool
+                monster.transform.position = new Vector3(transform.position.x, 2,0); // Set vị trí của trap
+                monster.SetActive(true); // Hiển thị trap lên màn hình
+                count = 0;
+            }
+        }if(random == 3)
+        {
+            int count = 0;
+            foreach (GameObject obj in listMine)
+            {
+                if (obj.active == false)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                GameObject monster = MinePool.GetObject(); // Lấy trap từ pool
+                monster.transform.position = new Vector3(transform.position.x, 2,0); // Set vị trí của trap
+                monster.SetActive(true); // Hiển thị trap lên màn hình
+                count = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/ThornPool.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornPool : MonoBehaviour
+{
+    public GameObject Thorn; // The prefab to be pooled.
+    public int poolSize = 5; // The number of instances to be created initially.
+    public List<GameObject> pool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(Thorn);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreach (GameObject obj in pool)
+        //{
+        //    if (!obj.activeInHierarchy)
+        //    {
+        //        obj.SetActive(false);
+        //    }
+        //}
+    }
+
+    public GameObject GetObject()
+    {
+        if (pool.Count == 0)
+        {
+            Debug.LogWarning("Object pool is empty!");
+            return null;
+        }
+        foreach (GameObject obj in pool)
+        {
+            if (obj.active == false)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
