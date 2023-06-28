9022da940ecd263b35f2f40058d56884f9883c45
NDT369 <92255832+NDT369@users.noreply.github.com>
2023-06-28 12:11:27 +0700

    update background

--- Assets/Scripts/Map/GroundController.cs
+++ Assets/Scripts/Map/GroundController.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class GroundController : MonoBehaviour
 {
-    float speed = 3;
-    float runTime;
-    float changeSpeedTime;
+   
     // Start is called before the first frame update
     void Start()
     {
-        runTime = 0;
-        changeSpeedTime= 15;
     }
 
     // Update is called once per frame
     void Update()
     {
-        runTime += Time.deltaTime;
-        if (runTime >= changeSpeedTime)
-        {
-            speed += 0.5f;
-            changeSpeedTime += 15;
-            Debug.Log(speed);
-        }
-        transform.position = Vector3.left * speed * Time.deltaTime + transform.position;
+        transform.position = Vector3.left * GameManage.speedGround * Time.deltaTime + transform.position;
     }
 }
