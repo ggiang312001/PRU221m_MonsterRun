f0861e0e812e8c069c6ea6706de6ae418decbfec
thanhpdhe150023@fpt.edu.vn <108501377+thanh23102001@users.noreply.github.com>
2023-07-12 10:12:15 +0700

    update

---
+++ Assets/Scripts/Map/GroundDestroyer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("SnowItem") || collision.gameObject.CompareTag("HUD") || collision.gameObject.CompareTag("Ground3") || collision.gameObject.CompareTag("Ground4"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
