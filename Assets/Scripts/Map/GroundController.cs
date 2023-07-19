ce82f1f5a766e812f25da96fffa14336f0c71a2a
NDT369 <92255832+NDT369@users.noreply.github.com>
2023-07-20 01:39:28 +0700

    setgroundactive

--- Assets/Scripts/Map/GroundController.cs
+++ Assets/Scripts/Map/GroundController.cs
@@ -15,4 +15,12 @@ public class GroundController : MonoBehaviour
     {
         transform.position = Vector3.left * GameManage.Instance.speed * Time.deltaTime + transform.position;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("GroundSpawner"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
