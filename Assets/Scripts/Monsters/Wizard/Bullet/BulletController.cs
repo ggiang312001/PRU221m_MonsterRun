63e9a0f4c0cf3868bc3e303d48b2e628ac70feef
NDT369 <92255832+NDT369@users.noreply.github.com>
2023-07-20 11:13:26 +0700

    setTriggerBullet

--- Assets/Scripts/Monsters/Wizard/Bullet/BulletController.cs
+++ Assets/Scripts/Monsters/Wizard/Bullet/BulletController.cs
@@ -7,11 +7,18 @@ public class BulletController : MonoBehaviour
 {
     private WizardExplosionPool WizardExplosionPool;
     HUD hud;
+    private Rigidbody2D rb2D;
     // Start is called before the first frame update
     void Start()
     {
         hud = GameObject.FindGameObjectWithTag("HUD").GetComponent<HUD>();
         WizardExplosionPool = FindObjectOfType<WizardExplosionPool>();
+        rb2D = GetComponent<Rigidbody2D>();
+        if (rb2D == null)
+            rb2D = gameObject.AddComponent<Rigidbody2D>();
+
+        // Disable gravity for the Rigidbody2D
+        rb2D.gravityScale = 0f;
     }
 
     // Update is called once per frame
@@ -20,7 +27,9 @@ public class BulletController : MonoBehaviour
         transform.position = Vector3.left * GameManage.Instance.speed * 2.5f * Time.deltaTime + transform.position;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
@@ -31,5 +40,14 @@ public class BulletController : MonoBehaviour
             explosion.transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, 0); // Set vị trí của trap
             explosion.SetActive(true);
         }
+        if (collision.gameObject.CompareTag("Shield"))
+        {
+            ShieldActive.Instance.ShieldCount -= 1;
+            gameObject.SetActive(false);
+            GameObject explosion = WizardExplosionPool.GetObject(); // Lấy trap từ pool
+            explosion.transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, 0); // Set vị trí của trap
+            explosion.SetActive(true);
+        }
     }
 }
+
